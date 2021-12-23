e807d52690ad4c9028c74c9b35ed36295ab3802e
cliffyb824 <qycliffy@gmail.com>
2021-12-22 18:30:18 -0600

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -24,8 +24,8 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-# engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('DisasterResponse', 'sqlite:///../data/DisasterResponse.db')
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
